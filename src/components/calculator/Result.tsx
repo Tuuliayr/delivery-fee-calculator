8beca9233ea5452fae8125368fcd44f9a779fadd
Tuuliayr <tuuliayravainen@gmail.com>
2024-01-30 16:01:22 +0200

    Create an element for displaying the result.

---
+++ src/components/calculator/Result.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Result = () => {
+    return (
+        <div className='circle-big'>
+            <div className='circle-small'>
+                Delivery price:
+            </div>
+        </div>
+    )
+}
+
+export default Result;
