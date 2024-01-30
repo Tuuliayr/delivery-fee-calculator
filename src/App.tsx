3691b6c458f8f38c454de76783c77f27cc6ba6da
Tuuliayr <tuuliayravainen@gmail.com>
2024-01-30 13:59:26 +0200

    Import and show Calculator.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import './App.css';
+import Calculator from './components/calculator/Calculator';
 
 function App() {
-  return (
-    <div className="App">
-     
-    </div>
-  );
+    return (
+        <div className="App">
+            <Calculator/>
+        </div>
+    );
 }
 
 export default App;
