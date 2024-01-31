b4a70200402e00b1c37d880627a18259577ef013
Tuuliayr <tuuliayravainen@gmail.com>
2024-01-31 16:24:03 +0200

    Create states for date and time input and calculated delivery fee.

--- src/components/calculator/Calculator.tsx
+++ src/components/calculator/Calculator.tsx
@@ -8,51 +8,45 @@ const Calculator = () => {
     const [cartVal, setCartVal] = useState("");
     const [distanceVal, setDistanceVal] = useState("");
     const [amountVal, setAmountVal] = useState("");
-    const [dateVal, setDateVal] = useState("");
-    const [timeVal, setTimeVal] = useState("");
-    const [deliveryFee, setDeliveryFee] = useState('');
+    const [dateTimeVal, setDateTimeVal] = useState("");
+    const [deliveryFee, setDeliveryFee] = useState("0");
 
     const handleSubmit = (e: React.SyntheticEvent) => {
-        setDeliveryFee(calculateFee(cartVal, distanceVal, amountVal, dateVal));
+        setDeliveryFee(calculateFee(cartVal, distanceVal, amountVal, dateTimeVal));
         e.preventDefault();
     }
 
     return (
-        <div className='calculator'>
-            <div className='title'>Calculate delivery fee</div>
+        <div className="calculator">
+            <div className="title">Calculate delivery fee</div>
             <form onSubmit={handleSubmit}>
-                <div className='fields'>
-                    <div className='field-names'>
-                        <label htmlFor='cart' className='field-name'>Cart value</label>
-                        <label htmlFor='distance' className='field-name'>Delivery distance</label>
-                        <label htmlFor='amount' className='field-name'>Amount of items</label>
-                        <label htmlFor='date' className='field-name'>Date</label>
-                        <label htmlFor='time' className='field-name'>Time</label>
+                <div className="fields">
+                    <div className="field-names">
+                        <label htmlFor="cart" className="field-name">Cart value</label>
+                        <label htmlFor="distance" className="field-name">Delivery distance</label>
+                        <label htmlFor="amount" className="field-name">Amount of items</label>
+                        <label htmlFor="date" className="field-name">Order time</label>
                     </div>
-                    <div className='field-values'>
-                        <div className='field-value'>
-                            <InputField data-test-id="cartValue" id='cart' type='number' value={cartVal} onChange={setCartVal}/>
+                    <div className="field-values">
+                        <div className="field-value">
+                            <InputField data-test-id="cartValue" id="cart" type="number" value={cartVal} onChange={setCartVal}/>
                             <div>€</div>
                         </div>
-                        <div className='field-value'>
-                            <InputField data-test-id="deliveryDistance" id='distance' type='number' value={distanceVal} onChange={setDistanceVal}/>
+                        <div className="field-value">
+                            <InputField data-test-id="deliveryDistance" id="distance" type="number" value={distanceVal} onChange={setDistanceVal}/>
                             <div>m</div>
                         </div>
-                        <div className='field-value'>
-                            <InputField data-test-id="numberOfItems" id='amount' type='number' value={amountVal} onChange={setAmountVal}/>
+                        <div className="field-value">
+                            <InputField data-test-id="numberOfItems" id="amount" type="number" value={amountVal} onChange={setAmountVal}/>
                         </div>
-                        {/* Combine date and time into one input */}
-                        <div className='field-value'>
-                            <InputField data-test-id="orderTime" id='date' type='date' value={dateVal} onChange={setDateVal}/>
-                        </div>
-                        <div className='field-value'>
-                            <InputField id='time' type='time' value={timeVal} onChange={setTimeVal}/>
+                        <div className="field-value">
+                            <InputField data-test-id="orderTime" id="date" type="datetime-local" value={dateTimeVal} onChange={setDateTimeVal}/>
                         </div>
                     </div>
                 </div>
-                <Button label='Calculate'/>
+                <Button label="Calculate"/>
             </form>
-            <Result price={deliveryFee + '€'}/>
+            <Result data-test-id="fee" fee={deliveryFee}/>
         </div>
     );
 }
